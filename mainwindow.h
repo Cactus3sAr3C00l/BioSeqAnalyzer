312cc93e1254246e3923019cb7d9abb4e2ce1cd6
Olimpia Gawrońska <ogawronskao@gmail.com>
2024-04-03 21:45:14 +0200

    Initialize git

---
+++ mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QMainWindow>
+#include <QPushButton>
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+public slots:
+    void chooseFile();    // Deklaracja funkcji wyboru pliku
+    void startAnalysis(); // Deklaracja funkcji rozpoczęcia analizy
+private:
+    QPushButton *fileButton; // Przycisk do wyboru pliku
+    QPushButton *analyzeButton;
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
